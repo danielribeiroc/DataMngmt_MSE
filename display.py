c364a7e5a1744b6e294480364d704e837f50571f
danielribeiroc <ribeiro.cabral@hotmail.com>
2023-11-17 20:08:13 +0100

    ADD : Exo 1 working

--- display.py
+++ display.py
@@ -16,7 +16,7 @@ def display_city_on_map(m, popup, latitude, longitude, radius=1000, color="#3186
 
 # display polyline on the folium map
 # locations: (list of points (latitude, longitude)) – Latitude and Longitude of line
-def display_polyline_on_map(m, locations, popup=None, color="#3186cc", weight=2.0):
+def display_polyline_on_map(m, locations, popup=None, color="#3186cc", weight=3.0):
     folium.PolyLine(
         locations,
         popup=popup,
@@ -40,6 +40,13 @@ class DisplayTrainNetwork:
             session.execute_read(self._display_cities, map_1)
         map_1.save('out/1.html')
 
+    def display_train_network(self):
+        map_1 = folium.Map(location=center_switzerland, zoom_start=8)
+        with self.driver.session() as session:
+            session.execute_read(self._display_cities, map_1)
+            session.execute_read(self._display_railway_lines, map_1)
+        map_1.save('out/train_network.html')
+
     @staticmethod
     def _display_cities(tx, m):
         query = (
@@ -57,6 +64,21 @@ class DisplayTrainNetwork:
                 longitude=record['c']['longitude']
             )
 
+    @staticmethod
+    def _display_railway_lines(tx, m):
+        query = (
+            """
+            MATCH (c1:City)-[r:RAILWAY]->(c2:City)
+            RETURN c1.name, c1.latitude, c1.longitude, c2.name, c2.latitude, c2.longitude
+            """
+        )
+        result = tx.run(query)
+        for record in result:
+            city1_coords = (record['c1.latitude'], record['c1.longitude']) # get coordinates of city 1
+            city2_coords = (record['c2.latitude'], record['c2.longitude']) # get coordinates of city 2
+            locations = [city1_coords, city2_coords] # regroup both
+            popup_text = f"Railway: {record['c1.name']} to {record['c2.name']}" # text to show
+            display_polyline_on_map(m=m, locations=locations, popup=popup_text) # draw line in map
 
 if __name__ == "__main__":
     display_train_network = DisplayTrainNetwork("neo4j://localhost:7687")
@@ -64,4 +86,5 @@ if __name__ == "__main__":
     center_switzerland = [46.800663464, 8.222665776]
 
     # display cities on the map
-    display_train_network.display_cities()
+    #display_train_network.display_cities()
+    display_train_network.display_train_network()
--- index.py
+++ index.py
@@ -1,5 +1,5 @@
-from neo4j import GraphDatabase
 import pandas as pd
+from neo4j import GraphDatabase
 
 
 class GenerateTrainNetwork:
@@ -31,13 +31,41 @@ class GenerateTrainNetwork:
             """
         )
         result = tx.run(query, name=name, latitude=latitude, longitude=longitude, population=population)
-
         city_created = result.single()['c']
         print("Created City: {name}".format(name=city_created['name']))
 
+    def create_railway_lines(self):
+        lines = pd.read_csv('data/lines.csv', sep=';')
+        for index, row in lines.iterrows():
+            with self.driver.session() as session:
+                session.execute_write(
+                    self._create_railway_line,
+                    row['city1'],
+                    row['city2'],
+                    row['km'],
+                    row['time'],
+                    row['nbTracks']
+                )
+
+    @staticmethod
+    def _create_railway_line(tx, city1, city2, km, time, nbTracks):
+        query = (
+            """
+            MATCH (c1:City {name: $city1}), (c2:City {name: $city2})
+            CREATE (c1)-[:RAILWAY {km: $km, time: $time, nbTracks: $nbTracks}]->(c2)
+            RETURN c1, c2
+            """
+        )
+        result = tx.run(query, city1=city1, city2=city2, km=km, time=time, nbTracks=nbTracks)
+        print(f"Created Railway Line from {city1} to {city2}")
+
 
 if __name__ == "__main__":
-    generate_train_network = GenerateTrainNetwork("neo4j://localhost:7687")
+    uri = "neo4j://localhost:7687"
+    generate_train_network = GenerateTrainNetwork(uri)
 
     # create all city nodes
     generate_train_network.create_cities()
+
+    # create all railway lines
+    generate_train_network.create_railway_lines()
